16d786a3c028e15424d4bdd35e7fa1cbc3de3421
Revenger333 <reynaldi525@gmail.com>
2021-06-11 08:59:12 +0700

    base

---
+++ view/layout.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <div id="header">
+        <div id="coklat"></div>
+        <div id="nama">Name</div>
+        
+    </div>
+    </body>
+</html>
+  
