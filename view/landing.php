6a2bd8fabd130da643c731583e90585569e0762b
Revenger333 <reynaldi525@gmail.com>
2021-06-13 22:18:03 +0700

    footer

--- view/landing.php
+++ view/landing.php
@@ -1,14 +1,60 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-</head>
-<body>
-    <!-- <?php
-        require 'navbarout.php';
-    ?> -->
-</body>
-</html>
+
+<div class="carousel-wrapper">
+        <div class="carousel">
+            <img class="carousel_photo initial" src="assets/293973.jpg" alt="">
+        </div>
+    </div>
+
+    <div>
+        SOMETHING
+    </div>
+
+    <div class="Grid_Container">
+        <div class="grid-item featurekiri">1</div>
+        <div class="grid-item desckiri">2</div>
+        <div class="grid-item desckanan">3</div>
+        <div class="grid-item featurekanan">4</div>
+        <div class="grid-item featurekiri">5</div>
+        <div class="grid-item desckiri">6</div>
+    </div>
+<div class="highlight">
+    popular track<br>
+    <div class="popular_track">
+        <div class="pop-item">
+            pop1
+        </div>
+
+        <div class="pop-item">
+            pop2
+        </div>
+
+        <div class="pop-item">
+            pop3
+        </div>
+
+        <div class="pop-item">
+            pop4
+        </div>
+    </div>
+</div>
+<br>
+<div class="highlight">
+    Region track<br>
+    <div class="region_track">
+        <div class="region-item">
+            Reg1
+        </div>
+
+        <div class="region-item">
+            Reg2
+        </div>
+
+        <div class="region-item">
+            Reg3
+        </div>
+
+        <div class="region-item">
+            Reg4
+        </div>
+    </div>
+</div>
--- view/layout/layout.php
+++ view/layout/layout.php
@@ -9,66 +9,15 @@
 </head>
 
 <body>
-    <div class="carousel-wrapper">
-        <div class="carousel">
-            <img class="carousel_photo initial" src="assets/293973.jpg" alt="">
-        </div>
+    <div id="header">HEADER</div>
+
+    <?php
+        echo $content
+    ?>
+
+    <div id="footer">
+         <h4>Virtual Marathon 2021, all rights not reserved</h4>
+         <h5 id="ig">instagram</h5>
     </div>
-
-    <div>
-        SOMETHING
-    </div>
-
-    <div class="Grid_Container">
-        <div class="grid-item featurekiri">1</div>
-        <div class="grid-item desckiri">2</div>
-        <div class="grid-item desckanan">3</div>
-        <div class="grid-item featurekanan">4</div>
-        <div class="grid-item featurekiri">5</div>
-        <div class="grid-item desckiri">6</div>
-    </div>
-<div class="highlight">
-    popular track<br>
-    <div class="popular_track">
-        <div class="pop-item">
-            pop1
-        </div>
-
-        <div class="pop-item">
-            pop2
-        </div>
-
-        <div class="pop-item">
-            pop3
-        </div>
-
-        <div class="pop-item">
-            pop4
-        </div>
-    </div>
-</div>
-<br>
-<div class="highlight">
-    Region track<br>
-    <div class="region_track">
-        <div class="region-item">
-            Reg1
-        </div>
-
-        <div class="region-item">
-            Reg2
-        </div>
-
-        <div class="region-item">
-            Reg3
-        </div>
-
-        <div class="region-item">
-            Reg4
-        </div>
-    </div>
-</div>
-    
 </body>
 </html>
-  
