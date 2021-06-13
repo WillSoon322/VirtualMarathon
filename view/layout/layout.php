1ecbdfd3269b094a650a7f6e5cc5a23803f1ad32
Revenger333 <reynaldi525@gmail.com>
2021-06-13 21:12:06 +0700

    landing skeleton MVC style

--- view/layout/layout.php
+++ view/layout/layout.php
@@ -1,25 +1,74 @@
 <html>
-    <head>
+   
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="view/style/landing_style.css">
+    <title>Document</title>
+</head>
 
-     <link href="style/style.css" type="text/css" rel="stylesheet" />
-        
-    </head>
-    <body>
-    <div id="header">
-        <div id="coklat">tes</div>
-        
-        
+<body>
+    <div class="carousel-wrapper">
+        <div class="carousel">
+            <img class="carousel_photo initial" src="assets/293973.jpg" alt="">
+        </div>
     </div>
 
-     <!-- <?php
-    //echo $content;
-    ?>   -->
-    
-
-    <div id="footer">
-    
-        
+    <div>
+        SOMETHING
     </div>
-    </body>
+
+    <div class="Grid_Container">
+        <div class="grid-item featurekiri">1</div>
+        <div class="grid-item desckiri">2</div>
+        <div class="grid-item desckanan">3</div>
+        <div class="grid-item featurekanan">4</div>
+        <div class="grid-item featurekiri">5</div>
+        <div class="grid-item desckiri">6</div>
+    </div>
+<div class="highlight">
+    popular track<br>
+    <div class="popular_track">
+        <div class="pop-item">
+            pop1
+        </div>
+
+        <div class="pop-item">
+            pop2
+        </div>
+
+        <div class="pop-item">
+            pop3
+        </div>
+
+        <div class="pop-item">
+            pop4
+        </div>
+    </div>
+</div>
+<br>
+<div class="highlight">
+    Region track<br>
+    <div class="region_track">
+        <div class="region-item">
+            Reg1
+        </div>
+
+        <div class="region-item">
+            Reg2
+        </div>
+
+        <div class="region-item">
+            Reg3
+        </div>
+
+        <div class="region-item">
+            Reg4
+        </div>
+    </div>
+</div>
+    
+</body>
 </html>
   
