811d5af1dd7a48800a4e2e1c63fbc41b4abf3968
Revenger333 <reynaldi525@gmail.com>
2021-06-15 07:43:26 +0700

    aaa

---
+++ controller/login_controller.php
@@ -0,0 +1,25 @@
+<?php
+ require_once "controller/services/mysqlDB.php";
+// require_once "model/book.php";
+ require_once "view/view.php";
+
+    class landingController{
+        
+        protected $db;
+
+        public function __construct (){
+            $this->db= new mySQLDB("localhost","root","","library");
+        }
+
+        public function viewAll(){
+            return View::createView("landing.php",[]);
+    
+        }
+
+       
+        }
+    
+   
+
+    
+?>
