927b8c6a41eab9e8b42253bbbaffc43d920b989a
flakyriemaid <valkkyriemaid@gmail.com>
2021-06-19 22:18:27 +0700

    AHHHH

--- view/JS/tracks.js
+++ view/JS/tracks.js
@@ -3,3 +3,11 @@ let accordion_button = document.querySelector('.region_accordion');
 accordion_button.addEventListener("click",function(){
     let panel = document.querySelector('.region_panel').classList.toggle("hide")
 })
+
+let card = document.querySelector('.grid_container').children;
+console.log(card);
+for(i=0;i<card.length;i++){
+    card[i].addEventListener("click",function(){
+        console.log("hai");
+    })
+}
--- view/trackpage.php
+++ view/trackpage.php
@@ -19,6 +19,7 @@
 
     <div class="content">
         <div class="top_part">
+            <div class="flag"><img src="view/assets/flag/japan.gif" alt=""></div>
             <img class="main_image" src="view/assets/current_track/fuji.jpg" alt="track">
             <div class="main_atribute">
                 <div class="main_badge"><img src="view/assets/badge/mountain1.png" alt="badge"></div>
@@ -28,7 +29,27 @@
         </div>
 
         <div class="bottom_part">
-            <div class="bottom_title"></div>
+            <div class="bottom_title">
+                <h1>Mt. Fuji - JAPAN</h1>
+            </div>
+            <br>
+            <hr>
+            <div class="bottom_desc">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad, aspernatur. Esse, ut, odit non nobis corporis quam quod, tempora adipisci eos cumque in! Perferendis reiciendis nisi ducimus eaque fugiat ipsa!
+                <br><br><br>
+                <hr>
+                <div>SOME OTHER RANDOM STUFF</div>
+                <div>SOME OTHER RANDOM STUFF</div>
+                <div>SOME OTHER RANDOM STUFF</div>
+                <div>SOME OTHER RANDOM STUFF</div>
+
+                <div class="get_track">
+                    GET THIS TRACK NOW
+                </div>
+            </div>
+            <div class="bottom_statistic">
+                statistic
+            </div>
         </div>
     </div>
 </body>
