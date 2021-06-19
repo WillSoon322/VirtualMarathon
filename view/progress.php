6562b54b0de4504bf261b2539bc5c4365be36181
flakyriemaid <valkkyriemaid@gmail.com>
2021-06-19 23:51:21 +0700

    progress page

---
+++ view/progress.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" href="view/style/progress.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="back_button">
+        BACK
+    </div>
+    <div class="flex_container">
+        <div class="logo">LOGO</div>
+        <div class="your">YOUR PROGRESS IN [TRACK]</div>
+        <div class="progress">74%</div>
+        <div class="progress_bar">BAR</div>
+        <button class="add_progress">WOI GOBLOK INI BUAT NAMBAH PROGRESS</button>
+    </div>
+</body>
+</html>
