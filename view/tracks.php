e86ef85709c1428782c2f4fb7f674a517d1de125
flakyriemaid <valkkyriemaid@gmail.com>
2021-06-25 10:13:15 +0700

    benerin navnya tracks

--- view/tracks.php
+++ view/tracks.php
@@ -4,26 +4,8 @@
 </head>
 
 <body>
-    <header>
-        <div class="logo">
-            <h1>LOGO</h1>
-        </div>
 
-        <?php
-        // foreach($result as $key =>$row){
-        //     echo $row->getGambar();
-        // }
-        // var_dump($result);
-
-        ?>
-
-        <nav>
-            <div class="nav_link"><a href="login"><b>Log In</b></a></div>
-            <div class="nav_link"><a href="signup"><b>Sign Up</b></a></div>
-
-        </nav>
-
-        <div class="track_utils">
+    <div class="track_utils">
             <fieldset>
                 <button class="region_accordion">Select Region</button>
                 <div class="region_panel">
@@ -71,20 +53,15 @@
                 }
             </script>
         </div>
-    </header>
 
     <div class="track_container">
         <div class="carousel">
-            <!--image slider start-->
             <div class="slider">
                 <div class="slides">
-                    <!--radio buttons start-->
                     <input type="radio" name="radio-btn" id="radio1">
                     <input type="radio" name="radio-btn" id="radio2">
                     <input type="radio" name="radio-btn" id="radio3">
                     <input type="radio" name="radio-btn" id="radio4">
-                    <!--radio buttons end-->
-                    <!--slide images start-->
                     <div class="slide first">
                         <img src="view/assets/1.jpg" alt="">
                     </div>
@@ -97,26 +74,20 @@
                     <div class="slide">
                         <img src="view/assets/4.jpg" alt="">
                     </div>
-                    <!--slide images end-->
-                    <!--automatic navigation start-->
                     <div class="navigation-auto">
                         <div class="auto-btn1"></div>
                         <div class="auto-btn2"></div>
                         <div class="auto-btn3"></div>
                         <div class="auto-btn4"></div>
                     </div>
-                    <!--automatic navigation end-->
                 </div>
-                <!--manual navigation start-->
                 <div class="navigation-manual">
                     <label for="radio1" class="manual-btn"></label>
                     <label for="radio2" class="manual-btn"></label>
                     <label for="radio3" class="manual-btn"></label>
                     <label for="radio4" class="manual-btn"></label>
                 </div>
-                <!--manual navigation end-->
             </div>
-            <!--image slider end-->
         </div>
         <div class="grid_container">
 
